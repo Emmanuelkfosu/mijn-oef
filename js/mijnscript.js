cdcb8f35253a50ed0ad6cda400306c4f333a6e36
Emmanuel Asante <kfosu22@icloud.com>
2020-05-11 12:28:34 +0200

    more

---
+++ js/mijnscript.js
@@ -0,0 +1,17 @@
+//$("h1").css("border", "1px solid Red");
+//$("h1").hide();
+//10000;
+$("ul li:first").css("color", "blue");
+$("ul li:last").css("background", "blue");
+$("ul li: odd").css("background: green");
+$("[href]").css("color", "blue");
+//$("a[href":http://yahoo.com"]").css("color", "green");
+$("ul").before("<h4>Shoping List</h4>");
+//$("ul").appendTo("<li>List item</li>");
+//$("p").hide("slow");didnt work pg 12$
+//$("ul").css({ "background-color": "blue" });didnt work
+//$("h2").prepend("<p>Registration</p>");
+$("button").click(functio("a").css("color", "red"));
+$(selector).click(function (e) {
+  e.preventDefault();
+});
